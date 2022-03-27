2755fee83a29eb0b80c18c1327ec170d8e0c002c
Andrew Wiltshire <addiktedmontage@gmail.com>
2022-03-27 19:13:05 +0100

    add sln file

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -3,11 +3,14 @@
 using BetterEventSystem;
 namespace Terminal;
 
-internal class Program
-{
-    static void Main(string[] args)
-    {
-        // instantiate all classes that contain events.
+internal class Program {
+    static void Main(string[] args) {
+        new Event("quit").AddListener((e) => {
+            Console.WriteLine("Quitting...");
+            Environment.Exit(0);
+        });
+        
+        // instantiate all classes that contain other events.
         new Help();
         new Blackjack();
         Settings settings = new Settings();
@@ -18,13 +21,13 @@ internal class Program
             string command = input[0]; // Get the first word of the input, which is the command
             string[] cmdArgs = input.Skip(1).ToArray(); // Get the rest of the input, which are the arguments
 
-            // Broadcast the event, passing the arguments as data.
-            // We are doing it synchronously, so we can wait for the command to finish before continuing.
             try {
+                // Try to find an event with the given name, and broadcast it while passing the arguments.
+                // We are doing it synchronously, so we can wait for the command to finish before continuing.
                 EventSystem.GetEvent(command, settings.CreateIfNotExists).BroadcastSync(data: cmdArgs);
             }
             catch (NullReferenceException) {
-                // if createIfNotExists is false, the event will not be created if it doesn't exist. this will throw a NullReferenceException, so we catch it and print an error message
+                // if Settings.createIfNotExists is false, the event will not be created if it doesn't exist. this will throw a NullReferenceException, so we catch it and print an error message
                 Console.WriteLine("Command not found.");
             }
 
